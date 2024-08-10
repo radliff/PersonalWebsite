4ec767d726405a665e73823783a80c2434bc1860
radliff <radliffjeantinor@gmail.com>
2024-08-09 22:21:07 -0400

    playlist done

--- portfolio/src/components/Journey.jsx
+++ portfolio/src/components/Journey.jsx
@@ -34,36 +34,75 @@ const Journey = () => {
           <div className="text-lg text-slate-400">Public Playlist</div>
           <div className="text-5xl text-white font-bold">Journey</div>
           <div className="text-lg text-white font-bold mt-5">
-            Radliff Jeantinor ⋅ <span className="text-sm text-slate-400">4 songs, 10 years 5 months</span>
+            Radliff Jeantinor ⋅ <span className="text-sm text-slate-400">4 songs, 2 years 6 months</span>
           </div>
         </div>
       </div>
       <div className="mt-10">
         <div className="grid grid-cols-5 text-lg text-slate-400 gap-x-8">
           <span>#</span>
-          <span className="text-center">Title</span>
+          <span className="text-left">Title</span>
           <span className="text-center">Skills</span>
           <span className="text-center">Date Started</span>
           <span className="text-center">Date Ended</span>
         </div>
         <div className="grid grid-cols-5 text-lg text-slate-400 mt-5 gap-x-8 items-center">
           <span>1</span>
-          <div className="flex justify-center">
-            <img src={nsbeLogo} alt="NSBE Logo" className="w-20 h-20 object-cover" />
-            <span className="text-white font-bold ml-5">National Society of Black Engineers</span>
-          </div>
-          <div className="flex flex-col items-center">
-            <span className="text-white font-bold">NSBE</span>
-            <span className="text-lg text-slate-400 mt-2">Academic Excellence Chair</span>
+          <div className="flex items-center">
+            <img src={nsbeLogo} alt="NSBE Logo" className="w-20 h-20 object-cover mr-4" />
+            <div>
+              <span className="text-white font-bold">NSBE</span>
+              <div className="text-lg text-slate-400 mt-2">Academic Excellence Chair</div>
+            </div>
           </div>
           <div className="text-center">Leadership</div>
-          <div className="text-center">January 2020</div>
-          <div className="text-center">May 2024</div>
+          <div className="text-center">May 6, 2024</div>
+          <div className="text-center">Present</div>
+        </div>
+        <div className="grid grid-cols-5 text-lg text-slate-400 mt-5 gap-x-8 items-center">
+          <span>2</span>
+          <div className="flex items-center">
+            <img src={ufLogo} alt="UF Logo" className="w-20 h-20 object-cover mr-4" />
+            <div>
+              <span className="text-white font-bold">UF</span>
+              <div className="text-lg text-slate-400 mt-2">Student</div>
+            </div>
+          </div>
+          <div className="text-center">Python, C++</div>
+          <div className="text-center">August 22, 2022</div>
+          <div className="text-center">Present</div>
+        </div>
+        <div className="grid grid-cols-5 text-lg text-slate-400 mt-5 gap-x-8 items-center">
+          <span>3</span>
+          <div className="flex items-center">
+            <img src={cmuLogo} alt="CMU logo" className="w-20 h-20 object-cover mr-4" />
+            <div>
+              <span className="text-white font-bold">Carnegie Mellon</span>
+              <div className="text-lg text-slate-400 mt-2">Summer Research Intern</div>
+            </div>
+          </div>
+          <div className="text-center">Pandas, ArcGIS</div>
+          <div className="text-center">May 28, 2023</div>
+          <div className="text-center">July 21, 2023</div>
+        </div>
+        <div className="grid grid-cols-5 text-lg text-slate-400 mt-5 gap-x-8 items-center">
+          <span>4</span>
+          <div className="flex items-center">
+            <img src={ukgLogo} alt="UKG logo" className="w-20 h-20 object-cover mr-4" />
+            <div>
+              <span className="text-white font-bold">UKG</span>
+              <div className="text-lg text-slate-400 mt-2">Software Engineering Intern</div>
+            </div>
+          </div>
+          <div className="text-center">MongoDB, Kafka, Springboot, Ansible, Java</div>
+          <div className="text-center">May 20, 2024</div>
+          <div className="text-center">August 16th, 2024</div>
         </div>
       </div>
     </div>
   );
 };
+
 
   
   export default Journey;
