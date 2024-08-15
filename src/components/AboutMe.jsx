cbd5eca27d717a915e751b3d81384666d3ace8b9
radliff <radliffjeantinor@gmail.com>
2024-08-15 19:11:59 -0400

    adjusted about me to be screen friendly

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,33 +7,35 @@ import smilingPic from '../img/smilingPic.jpg'
 
 const AboutMe = () => {
   return (
-    <div className='w-full bg-cover bg-center relative' style={{ backgroundImage: `url(${smilingPic})`, backgroundPosition: 'left 40% top 30%', height: '60vh' }}>
+    <div className='w-full bg-cover bg-center relative h-60 md:h-70 lg:h-96' 
+         style={{ backgroundImage: `url(${smilingPic})`, backgroundPosition: 'left 40% top 30%' }}>
       <div className='absolute inset-0 bg-black opacity-50'></div>
       <div className='relative z-10 flex justify-left items-center h-full'>
-        <div className='relative px-10'>
+        <div className='relative px-5 sm:px-10'>
           <img 
             src={verifiedBadge} 
             alt="Verified Badge" 
-            className='absolute w-9 h-8' 
-            style={{ top: '-20px', left: '30px' }} 
+            className='absolute w-7 h-6 sm:w-9 sm:h-8' 
+            style={{ top: '-15px', left: '20px' }} 
           />
-          <h2 className='text-white text-md px-7' style={{ transform: 'translateY(-15px)' }}>
+          <h2 className='text-white text-sm sm:text-md px-7 transform -translate-y-3 sm:-translate-y-4'>
             Verified Artist
           </h2>
-          <h1 className='text-white text-5xl font-bold text-left'>Radliff :)</h1>
+          <h1 className='text-white text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-bold text-left'>
+            Radliff :)
+          </h1>
         </div>
       </div>
-      <div className='absolute top-0 right-10 m-4 z-20 text-white text-xl max-w-md p-4 bg-opacity-75 bg-black border-4 border-[#1DB954] rounded-lg'>
-        <p>
+      <div className='absolute top-0 right-4 sm:right-10 m-2 sm:m-4 z-20 text-white text-base sm:text-xl md:text-lg lg:text-xl max-w-xs sm:max-w-xs p-2 sm:p-4 bg-opacity-75 bg-black border-2 sm:border-4 border-[#1DB954] rounded-lg'>
+        <p className="text-xs sm:text-sm md:text-base lg:text-lg leading-tight">
           Hey there! I'm Radliff Jeantinor, a student with a passion for tech. <br />
           Welcome to my Spotify-themed corner of the web, where I mix my love for code with my favorite app. <br/>
-          Just like a good playlist, I believe a great website should be a blend of creativity, functionality, and personality. <br/> 
-          Here, you'll find a collection of my projects, and a glimpse into my journey as a Black engineer. <br/>
-          Thanks for stopping by—explore!
+          Just like a good playlist, I believe a great website should be a blend of creativity, functionality, and personality. <br/>
         </p>
       </div>
     </div>
-  )
+  );
 }
+
 
 export default AboutMe
